b1ea5639e5fd7d5e3004345cbf8b1a6383afcf00
Brett-Meirhofer <78938784+BrettMeirhofer@users.noreply.github.com>
2021-12-14 23:08:38 -0600

    Moved DATABASE info to private file

--- TechspireSite/TechspireSite/settings.py
+++ TechspireSite/TechspireSite/settings.py
@@ -82,12 +82,7 @@ WSGI_APPLICATION = 'TechspireSite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+DATABASES = SiteVars.DATABASES
 
 
 # Password validation
